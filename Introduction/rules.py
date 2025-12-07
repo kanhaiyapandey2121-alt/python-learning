d99e483e83f4e3feab361b7ff0d29164aa7e7e4a
kanhaiyapandey2121-alt <kanhaiyapandey2121@gmail.com>
2025-12-07 08:47:47 +0000

    rules

---
+++ Introduction/rules.py
@@ -0,0 +1,4 @@
+name = "kaanhaiyapandey" #name se pahle koi bhi number ya specific symbol use nahi kar sakte sirf underscore ko chhod kar 
+age = 21
+height = 6
+print (name,age,height)
--- basics/basic3.py
+++
@@ -1,4 +0,0 @@
-a=3
-b=6
-sum=a+b
-print(sum)
