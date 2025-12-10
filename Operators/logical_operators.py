6f99ebf3e19ae14a6adc456ec9f4545cfc655fc2
kanhaiyapandey2121-alt <kanhaiyapandey2121@gmail.com>
2025-12-10 12:59:07 +0000

    oprerators

---
+++ Operators/logical_operators.py
@@ -0,0 +1,16 @@
+"""
+multiple condition combine-boolean true/false
+
+and-all conditions must be true>true
+or-atleat one condition true>true
+not-singal operand,reverse 
+"""
+age=20
+is_student= True
+
+
+print(age>18 and is_student)
+
+print(age>25 or is_student)
+
+print(not is_student)
