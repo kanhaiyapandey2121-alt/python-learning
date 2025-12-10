2a78cc1c9f65f930f834bbd34ebd6ab53c0bf00a
kanhaiyapandey2121-alt <kanhaiyapandey2121@gmail.com>
2025-12-10 13:06:10 +0000

    operators

--- Operators/logical_operators.py
+++ Operators/logical_operators.py
@@ -8,9 +8,14 @@ not-singal operand,reverse
 age=20
 is_student= True
 
-
 print(age>18 and is_student)
-
 print(age>25 or is_student)
-
 print(not is_student)
+
+#or one more example
+
+a=10
+b=20
+print(a>5 and b>10)
+print(a<2 or b==20)
+print(not(a==10))
