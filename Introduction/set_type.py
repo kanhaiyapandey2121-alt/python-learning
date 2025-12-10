c5570209e39ea39be760f2bd095f0b4cf5b999d9
kanhaiyapandey2121-alt <kanhaiyapandey2121@gmail.com>
2025-12-10 07:12:34 +0000

    set_type

---
+++ Introduction/set_type.py
@@ -0,0 +1,7 @@
+# 1.set(mutable)data change kar sakte hai 
+#isme uique value print hoti hai
+
+#2frozenset(immutable)means data change nhi kar sakte isme
+#isme bhi value repeat nhi hoti aur isme data change nhi kar sakte 
+unique_number={1,1,2,2,2,2,3,3,4,4,5,5,5,6,7}
+print(unique_number)
