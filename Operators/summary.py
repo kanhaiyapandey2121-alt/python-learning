14f8c197365c9a899a290e8212b842e92ade799d
kanhaiyapandey2121-alt <kanhaiyapandey2121@gmail.com>
2025-12-10 13:59:41 +0000

    operartors

---
+++ Operators/summary.py
@@ -0,0 +1,10 @@
+"""
+airthmetic-math calculations 
+comparison-compare values 
+logical-mutiple condition combine 
+assignment-assign values 
+identity-object memory location 
+membership-value exist in sequences 
+
+
+"""
