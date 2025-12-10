ac875742065ca2febc874ba479359502c748ae22
kanhaiyapandey2121-alt <kanhaiyapandey2121@gmail.com>
2025-12-10 13:33:13 +0000

    operators

---
+++ Operators/assignment_operator.py
@@ -0,0 +1,11 @@
+"""
+=    asignment operator
++=    addition assignment operator
+-=     subtraction assignment operator 
+*=      multiplication assignment operator
+"""
+#example
+x=10
+x+=5
+print(x)
+# isse ham kisi number mai+ addition,subtraction,multiplication sab kar sakte hai 
---
+++ Operators/identiity_operators.py
@@ -0,0 +1,12 @@
+#do object ki memory location compare karne ke liye identity operator ka use karte hai 
+"""
+
+is-True
+is not -True
+
+"""
+a=[1,2,3]
+b= a
+c=[1,2,3]
+print(a is b)
+print(a is c)
