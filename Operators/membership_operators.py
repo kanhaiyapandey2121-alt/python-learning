f10f7cb27178380714b73e6e27703daee8b0411d
kanhaiyapandey2121-alt <kanhaiyapandey2121@gmail.com>
2025-12-10 13:51:34 +0000

    operators

---
+++ Operators/membership_operators.py
@@ -0,0 +1,10 @@
+#to check weather a value exist in sequence or not
+"""
+isme do operator hote hai 
+in- true
+not in-true
+
+"""
+vegetables=['karela','aloo','tamatar']
+print('aloo'in vegetables )
+print('bhindi'in vegetables)
